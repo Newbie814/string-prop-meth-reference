a6965f5b157d1764429a690840d9b8f694f4eb69
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-22 20:55:28 -0400

    inital commit

---
+++ app.js
@@ -0,0 +1,30 @@
+let text = 'Matthew Woodard';
+
+console.log(text.length); // 15  counts white space as well
+
+console.log(text.toLowerCase()); // matthew woodard
+console.log(text.toUpperCase()); // MATTEW WOODARD
+
+console.log(text.charAt(3)); // t
+console.log(text.charAt(text.length - 1)); // d  to get last character of a string
+console.log(text.indexOf('w')); //6 returns index of first instance
+console.log(text);
+console.log(text.trim()); // no change, as it only removes leading and trailing white space
+console.log(text.startsWith('M')); // true
+console.log(text.startsWith('m')); // false - case sensitive
+console.log(text);
+console.log(text);
+
+// const person = {
+//   name: 'Matthew', //property
+//   greeting() {
+//     //method
+//     console.log(`Hello ${this.name}`);
+//   },
+// };
+
+// console.log(person);
+
+// console.log(person.name); // Matthew
+
+// person.greeting(); // Hello Matthew
